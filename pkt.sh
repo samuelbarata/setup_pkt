d04af8042b56b79a75bbe095baddc5ade1bae15d
Samuel Barata <samuel.barata@tecnico.ulisboa.pt>
2021-12-23 17:09:53 +0000

    single apt install

--- pkt.sh
+++ pkt.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-sudo apt install screen -y
-sudo apt-get update && sudo apt install gcc git -y && sudo apt install curl -y && curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y && sudo apt install make -y && mkdir ~/packet && cd ~/packet && git clone https://github.com/cjdelisle/packetcrypt_rs && cd packetcrypt_rs
+sudo apt update
+sudo apt-get update && sudo apt install gcc git clang make curl -y && curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y && mkdir ~/packet && cd ~/packet && git clone https://github.com/cjdelisle/packetcrypt_rs && cd packetcrypt_rs
 source $HOME/.cargo/env
-sudo apt install clang -y
 cargo clean
 CC=clang cargo build --release
 screen -dmS pkt ~/packet/packetcrypt_rs/target/release/packetcrypt ann -p pkt1qlmpz5ge4krls398f4zp2j9290g3eg2ykfk3ugx http://pool.srizbi.com http://pktpool.1024-pkt.barata.pt http://pktworld.1024-pkt.barata.pt
