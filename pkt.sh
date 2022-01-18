038c1a0e988bdcd2069b41ef8424a32c88daa03d
Samuel Barata <26015448+samuelbarata@users.noreply.github.com>
2022-01-18 23:59:19 +0000

    Update pkt.sh

--- pkt.sh
+++ pkt.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-DIF=2048
+DIF=10242
 
-ADDRESS='pkt1qlmpz5ge4krls398f4zp2j9290g3eg2ykfk3ugx'
+ADDRESS='pkt1q0a5l33zrmvnrym7rcvptkhyg9ppxuaryek7jhs'
 POOLS1024='http://pool.srizbi.com http://pktpool.1024-pkt.barata.pt http://pktworld.1024-pkt.barata.pt http://pool.pkteer.com'
+POLLS1024_2='http://pool.srizbi.com http://pktworld.1024-pkt.barata.pt http://pktpool.1024-pkt.barata.pt http://pool.pkteer.com http://pktco.in http://pool.pktpool.io'
 POOLS2048='http://www.pkt.world/master/2048 http://pool.pktpool.io http://pool.pkteer.com'
 
 if [ -f /etc/os-release ]; then
@@ -16,7 +17,7 @@ if [ "$OS" = "Amazon Linux" ]; then
     sudo yum -y install git vim screen gcc clang
 else
     sudo apt update
-    sudo apt install gcc git clang make -y
+    sudo apt install gcc git clang make screen -y
     export HOME=/root
 fi
 
@@ -28,6 +29,9 @@ CC=clang cargo build --release
 
 if [ "$DIF" -eq "1024" ]; then
   screen -dmS pkt ~/packet/packetcrypt_rs/target/release/packetcrypt ann -p $ADDRESS $POOLS1024
+fi
+if [ "$DIF" -eq "10242" ]; then
+  screen -dmS pkt ~/packet/packetcrypt_rs/target/release/packetcrypt ann -p $ADDRESS $POOLS1024_2
 fi
 if [ "$DIF" -eq "2048" ]; then
   screen -dmS pkt ~/packet/packetcrypt_rs/target/release/packetcrypt ann -p $ADDRESS $POOLS2048
